90d521236f52c9f5952b22dbdbbb03dcb34b3939
joostbooy <joostbooy@gmail.com>
2024-03-03 09:09:27 +0100

    instantiate instruments & samples

--- Src/Settings/settings.h
+++ Src/Settings/settings.h
@@ -1,6 +1,8 @@
 #ifndef Settings_h
 #define Settings_h
 
+#include "sample.h"
+#include "instrument.h"
 #include "stringBuilder.h"
 
 class Settings {
@@ -8,9 +10,22 @@ class Settings {
 public:
 
 	void init() {
+		for (size_t i = 0; i < kMaxSamples; ++i) {
+			sample(i).init();
+		}
 
+		for (size_t i = 0; i < kMaxInstruments; ++i) {
+			instrument(i).init();
+		}
 	}
 
+	Sample &sample(size_t index) {
+		return sample_[index];
+	}
+
+	Instrument &instrument(size_t index) {
+		return instrument_[index];
+	}
 
 	// save & load
 	bool save();
@@ -31,6 +46,12 @@ public:
 
 private:
 	StringBuilderBase<63>path;
+
+	static const size_t kMaxSamples = 128;
+	static const size_t kMaxInstruments = 4;
+	Sample sample_[kMaxSamples];
+	Instrument instrument_[kMaxInstruments];
+
 };
 
 extern Settings settings;
