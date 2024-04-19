9f685ebe75251ec346a3401b55f07fd21f6c2e8e
joostbooy <joostbooy@gmail.com>
2024-04-19 07:28:52 +0200

    refactored sampleQue

--- Src/Engine/sampleQue.h
+++ Src/Engine/sampleQue.h
@@ -10,23 +10,13 @@ class SampleQue {
 public:
 
 	struct Event {
-		Instrument * instrument_;
+		Instrument *instrument_;
 		Sample *sample_;
 		MidiEngine::Event midi_event_;
 	};
 
 	void init() {
 		event_que_.init();
-	}
-
-	size_t note_on(MidiEngine::Event &e) {
-		size_t last_size = event_que_.size();
-
-		for (size_t i = 0; i < Settings::kMaxInstruments; ++i) {
-			poll(&settings.instrument(i), e);
-		}
-
-		return event_que_.size() - last_size;
 	}
 
 	bool readable() {
@@ -37,27 +27,28 @@ public:
 		return event_que_.read();
 	}
 
+	size_t note_on(MidiEngine::Event &e) {
+		size_t last_size = event_que_.size();
+
+		for (size_t i = 0; i < Settings::kMaxInstruments; ++i) {
+			write(&settings.instrument(i), e);
+		}
+
+		return event_que_.size() - last_size;
+	}
+
 private:
 	Que<Event, 8>event_que_;
 
-	void poll(Instrument *instrument, MidiEngine::Event &e) {
+	void write(Instrument *instrument, MidiEngine::Event &e) {
 		if (instrument->midi_accepted(e)) {
 			for (size_t i = 0; i < instrument->num_samples(); ++i) {
-				if (instrument->sample(i)->within_key_range(e.data[0])) {
-					write(instrument, instrument->sample(i), e);
+				if (instrument->sample(i)->within_key_range(e.data[0]) && event_que_.writeable()) {
+					event_que_.write( { .instrument_ = instrument, .sample_ = instrument->sample(i), .midi_event_ = e } );
 				}
 			}
 		}
 	}
-
-	bool write(Instrument *instrument, Sample *sample, MidiEngine::Event &e) {
-		if (event_que_.writeable()) {
-			event_que_.write( { .instrument_ = instrument, .sample_ = sample, .midi_event_ = e } );
-			return true;
-		}
-		return false;
-	}
-
 };
 
 #endif
