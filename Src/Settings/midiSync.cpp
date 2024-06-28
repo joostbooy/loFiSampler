ba78aaa46274c293d507d9b3f03d4ca74c297af8
joostbooy <joostbooy@gmail.com>
2024-06-28 08:09:20 +0200

    refactored tempo multiplier

--- Src/Settings/midiSync.cpp
+++
@@ -1,3 +0,0 @@
-#include "midiSync.h"
-
-const float MidiSync::tempo_type_multiplier_[MidiSync::NUM_TYPES] = { 1.f / 1.5f, 1.f, 1.5f };
--- Src/Settings/midiSync.h
+++ Src/Settings/midiSync.h
@@ -40,11 +40,23 @@ public:
 	static float read_inc(int value) {
 		int type = value % 3;
 		int divisor = (value / 3) + 1;
-		return divisor * lut_tempo_sync_phase_inc[MidiClockEngine::bpm()] * tempo_type_multiplier_[type];
+		return divisor * lut_tempo_sync_phase_inc[MidiClockEngine::bpm()] * tempo_type_multiplier(type);
 	}
 
 private:
-	static const float tempo_type_multiplier_[NUM_TYPES];
+
+	static float tempo_type_multiplier(int type) {
+		switch (type)
+		{
+		case DOTTED:	return 1.f / 1.5f;
+		case WHOLE:		return 1.f;
+		case TRIPLET:	return 1.5f;
+		default:
+			break;
+		}
+		return 0.f;
+	}
+
 };
 
 #endif
