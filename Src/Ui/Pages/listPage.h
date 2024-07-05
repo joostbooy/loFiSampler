849dc254d3c26432d899d688adfd9ad199e706cf
joostbooy <joostbooy@gmail.com>
2024-07-05 07:04:54 +0200

    implemented list page

--- Src/Ui/Pages/listPage.h
+++ Src/Ui/Pages/listPage.h
@@ -1,15 +1,15 @@
 #ifndef ListPage_h
 #define ListPage_h
 
-//#include "page.h"
+#include "confirmationPage.h"
 #include "settingsList.h"
 
 namespace ListPage {
 
 	SettingsList *list_;
-	void(*clear_callback_)();
-	bool(*paste_callback_)();
-	void(*copy_callback_)();
+	void(*clear_callback_)() = nullptr;
+	bool(*paste_callback_)() = nullptr;
+	void(*copy_callback_)() = nullptr;
 
 	void set_list(SettingsList *list) {
 		list_ = list;
@@ -45,90 +45,91 @@ namespace ListPage {
 	}
 
 	void on_button(int id, int state) {
+		if (!state) {
+			return;
+		}
+
+		switch (id)
+		{
+		case Controller::UP_BUTTON:
+			list_->on_up_button();
+			break;
+		case Controller::DOWN_BUTTON:
+			list_->on_down_button();
+			break;
+		case Controller::CLEAR_BUTTON:
+			if (clear_callback_) {
+				ConfirmationPage::set("CLEAR SETTINGS ?", [](int option) {
+					if (option == ConfirmationPage::CONFIRM) {
+						clear_callback_();
+					}
+				});
+				TopPage::pages_->open(Pages::CONFIRMATION_PAGE);
+			}
+			break;
+		case Controller::COPY_BUTTON:
+			if (copy_callback_) {
+				copy_callback_();
+				MessagePainter::show("SETTINGS COPIED");
+			}
+			break;
+		case Controller::PASTE_BUTTON:
+			if (paste_callback_) {
+				ConfirmationPage::set("OVERWRITE SETTINGS ?", [](int option) {
+					if (option == ConfirmationPage::CONFIRM) {
+						if (paste_callback_()) {
+							MessagePainter::show("SETTINGS PASTED");
+						} else {
+							MessagePainter::show("FAILED! CLIPBOARD EMPTY");
+						}
+					}
+				});
+				TopPage::pages_->open(Pages::CONFIRMATION_PAGE);
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
+	void on_encoder(int id, int state) {
+		int index = Controller::encoder_to_function(id);
+		if (index >= 0) {
+			bool pressed = Controller::encoder_is_pressed(id);
+			bool shifted = Controller::is_pressed(Controller::SHIFT_BUTTON);
+			list_->on_encoder(index, state, pressed || shifted);
+		}
+	}
+
+	void refresh_leds() {
+		LedPainter::set_shift(Matrix::RED);
+
+		if (clear_callback_) {
+			LedPainter::set_clear(Matrix::RED);
+		}
+
+		if (copy_callback_ && paste_callback_) {
+			LedPainter::set_copy(Matrix::RED);
+			LedPainter::set_paste(Matrix::RED);
+		}
+	}
+
+	void draw() {
 		/*
-		if (id == Controller::UP_BUTTON && value > 0) {
-		list_->on_up_button();
-		return;
+		WindowPainter::draw_header();
+
+		Canvas::Color color;
+		TopPage::canvas_->set_font(Font::SMALL);
+		int item = list_->top_item();
+
+		for (int i = window.row().first; i <= window.row().last; ++i) {
+		color = (item == i) ? Canvas::BLACK : Canvas::LIGHT_GRAY;
+		WindowPainter::text(window.cell(0, i), list_->item_text(i), Canvas::LEFT, Canvas::CENTER, color);
+		WindowPainter::text(window.cell(1, i), list_->value_text(i), Canvas::LEFT, Canvas::CENTER, color);
 	}
 
-	if (id == Controller::DOWN_BUTTON && value > 0) {
-	list_->on_down_button();
-	return;
-}
-
-if (id == Controller::CLEAR_BUTTON && value >= 1 && clear_callback_ != nullptr) {
-ConfirmationPage::set("CLEAR ALL SETTINGS ?", [](uint8_t option) {
-if (option == ConfirmationPage::CONFIRM) {
-clear_callback_();
-}
-});
-TopPage::ui_->pages().open(Pages::CONFIRMATION_PAGE);
-return;
-}
-
-if (id == Controller::COPY_BUTTON && value >= 1 && copy_callback_ != nullptr) {
-copy_callback_();
-MessagePainter::show("SETTINGS COPIED");
-return;
-}
-
-if (id == Controller::PASTE_BUTTON && value >= 1 && paste_callback_ != nullptr) {
-ConfirmationPage::set("OVERWRITE SETTINGS ?", [](uint8_t option) {
-if (option == ConfirmationPage::CONFIRM) {
-if (paste_callback_()) {
-MessagePainter::show("SETTINGS PASTED");
-} else {
-MessagePainter::show("FAILED! CLIPBOARD EMPTY");
-}
-}
-});
-TopPage::ui_->pages().open(Pages::CONFIRMATION_PAGE);
-return;
-}
-*/
-}
-
-void on_encoder(int id, int state) {
-	/*
-	int index = Controller::encoder_to_function(id);
-	if (index >= 0) {
-	bool pressed = Controller::encoder_is_pressed(index);
-	bool shifted = Controller::is_pressed(Controller::SHIFT_BUTTON);
-	list_->on_encoder(index, inc, pressed || shifted);
-}
-*/
-}
-
-void refresh_leds() {
-	if (clear_callback_) {
-		LedPainter::set_clear(Matrix::GREEN);
-	}
-
-	LedPainter::set_y(Matrix::GREEN);
-	LedPainter::set_edit(Matrix::GREEN);
-	LedPainter::set_menu(Matrix::GREEN);
-
-	if (copy_callback_ && paste_callback_) {
-		LedPainter::set_copy(Matrix::GREEN);
-	}
-}
-
-void draw() {
-	/*
-	WindowPainter::draw_header();
-
-	Canvas::Color color;
-	TopPage::canvas_->set_font(Font::SMALL);
-	int item = list_->top_item();
-
-	for (int i = window.row().first; i <= window.row().last; ++i) {
-	color = (item == i) ? Canvas::BLACK : Canvas::LIGHT_GRAY;
-	WindowPainter::text(window.cell(0, i), list_->item_text(i), Canvas::LEFT, Canvas::CENTER, color);
-	WindowPainter::text(window.cell(1, i), list_->value_text(i), Canvas::LEFT, Canvas::CENTER, color);
-}
-
-WindowPainter::vertical_scrollbar(window);
-*/
+	WindowPainter::vertical_scrollbar(window);
+	*/
 }
 
 const size_t target_fps() {
