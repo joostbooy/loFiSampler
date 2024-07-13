92b4af2628c1ba5e21e9b3b4b003e16ea802c687
joostbooy <joostbooy@gmail.com>
2024-07-13 08:46:39 +0200

    cleanup

--- Drivers/system.cpp
+++ Drivers/system.cpp
@@ -74,10 +74,10 @@ void Sys::init() {
 	HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
 
 	// Interupt priorities
-		HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
-		HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);	// DAC
-		HAL_NVIC_SetPriority(TIM3_IRQn, 1, 0);			// Engine tick
-		HAL_NVIC_SetPriority(TIM7_IRQn, 2, 0);			// Ui Poll
-		HAL_NVIC_SetPriority(DMA2_Stream3_IRQn, 3, 0);	// SDIO
-		HAL_NVIC_SetPriority(DMA2_Stream4_IRQn, 4, 0);	// LCD
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+	HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);	// DAC
+	HAL_NVIC_SetPriority(TIM3_IRQn, 1, 0);			// Engine tick
+	HAL_NVIC_SetPriority(TIM7_IRQn, 2, 0);			// Ui Poll
+	HAL_NVIC_SetPriority(DMA2_Stream3_IRQn, 3, 0);	// SDIO
+	HAL_NVIC_SetPriority(DMA2_Stream4_IRQn, 4, 0);	// LCD
 }
