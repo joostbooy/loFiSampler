6816888bcb8e0a212104c66efc8f0dc265dd7117
joostbooy <joostbooy@gmail.com>
2024-07-17 12:37:05 +0200

    made delay stereo

--- Src/Engine/delayEngine.h
+++ Src/Engine/delayEngine.h
@@ -1,24 +1,33 @@
 #ifndef DelayEngine_h
 #define DelayEngine_h
 
-#include "delay.h"
+#include "settings.h"
+#include <algorithm>
 
 class DelayEngine {
 
 public:
 
-	void init(Delay *delay) {
-		delay_ = delay;
+	void init(Settings *settings) {
 		pos_ = 0;
-		std::fill(delay_line_[0], delay_line_[delay_line_size_], 0);
+		delay_ = &settings->delay();
+		std::fill(&delay_l_[0], &delay_l_[delay_line_size_], 0);
+		std::fill(&delay_r_[0], &delay_r_[delay_line_size_], 0);
 	}
 
-	void process(int16_t *buffer, size_t size) {
+	void process(int16_t *left, int16_t *right, size_t size, size_t inc) {
 		while (size--) {
-			int16_t input = *buffer;
-			delay_line_[pos_] = input + (delay_line_[pos_] * delay_->feedback());
+			int16_t l = *left;
+			int16_t r = *right;
 
-			*buffer++ = Dsp::cross_fade(input, delay_line_[pos_], delay_->mix());
+			delay_l_[pos_] = l + (delay_l_[pos_] * delay_->feedback());
+			delay_r_[pos_] = r + (delay_r_[pos_] * delay_->feedback());
+
+			*left = Dsp::cross_fade(l, delay_l_[pos_], delay_->mix());
+			*right = Dsp::cross_fade(r, delay_r_[pos_], delay_->mix());
+
+			left += inc;
+			right += inc;
 
 			if (++pos_ >= (delay_->amount() * delay_line_size_)) {
 				pos_ = 0;
@@ -30,7 +39,8 @@ private:
 	Delay *delay_;
 	size_t pos_;
 	size_t delay_line_size_ = SAMPLE_RATE; // 1 second
-	int16_t *delay_line_;
+	int16_t *delay_l_;
+	int16_t *delay_r_;
 };
 
 #endif
