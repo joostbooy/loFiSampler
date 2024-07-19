ede3c5f29c9138051a817317d2a1ef9d402d01dd
joostbooy <joostbooy@gmail.com>
2024-07-19 07:42:39 +0200

    optimized delay engine for cache lines

--- Src/Engine/delayEngine.h
+++ Src/Engine/delayEngine.h
@@ -15,25 +15,30 @@ public:
 		std::fill(&delay_r_[0], &delay_r_[Delay::kMaxDelay], 0);
 	}
 
+
 	void process(Dac::Buffer *buffer, size_t size) {
 		int channel = delay_->channel() * 2;
 		int16_t *left = &buffer[0].channel[channel];
 		int16_t *right = &buffer[0].channel[channel + 1];
 
+		float mix = delay_->mix();
+		float feedback = delay_->feedback();
+		size_t num_samples = delay_->num_samples();
+
 		while (size--) {
 			int16_t l = *left;
 			int16_t r = *right;
 
-			delay_l_[pos_] = l + (delay_l_[pos_] * delay_->feedback());
-			delay_r_[pos_] = r + (delay_r_[pos_] * delay_->feedback());
+			delay_l_[pos_] = l + (delay_l_[pos_] * feedback);
+			delay_r_[pos_] = r + (delay_r_[pos_] * feedback);
 
-			*left = Dsp::cross_fade(l, delay_l_[pos_], delay_->mix());
-			*right = Dsp::cross_fade(r, delay_r_[pos_], delay_->mix());
+			*left = Dsp::cross_fade(l, delay_l_[pos_], mix);
+			*right = Dsp::cross_fade(r, delay_r_[pos_], mix);
 
 			left += Dac::kNumChannels;
 			right += Dac::kNumChannels;
 
-			if (++pos_ >= delay_->num_samples()) {
+			if (++pos_ >= num_samples) {
 				pos_ = 0;
 			}
 		}
