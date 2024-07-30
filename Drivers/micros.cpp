291e98f90e0e23f96b7eb64aff39bfab1b0f8734
joostbooy <joostbooy@gmail.com>
2024-07-30 09:20:48 +0200

    fixed micros driver

--- Drivers/micros.cpp
+++ Drivers/micros.cpp
@@ -2,6 +2,8 @@
 
 void Micros::init() {
 	TIM_HandleTypeDef TimHandle;
+	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+	TIM_MasterConfigTypeDef sMasterConfig = {0};
 
 	TimHandle.Instance = TIM5;
 	TimHandle.Init.Prescaler = (F_CPU / 1000000 / 4) - 1;
@@ -9,7 +11,14 @@ void Micros::init() {
 	TimHandle.Init.Period = 0xFFFFFFFF;
 	TimHandle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 	TimHandle.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
-
 	HAL_TIM_Base_Init(&TimHandle);
+
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	HAL_TIM_ConfigClockSource(&TimHandle, &sClockSourceConfig);
+
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	HAL_TIMEx_MasterConfigSynchronization(&TimHandle, &sMasterConfig);
+
 	HAL_TIM_Base_Start(&TimHandle);
 }
