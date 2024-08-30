8259b504c593c5d3bf3faa08edbe11ff5589069d
joostbooy <joostbooy@gmail.com>
2024-08-30 12:01:18 +0200

    added import all & import all as mono

--- Src/Ui/Pages/wavImportPage.h
+++ Src/Ui/Pages/wavImportPage.h
@@ -11,34 +11,51 @@ namespace WavImportPage {
 	enum Options {
 		IMPORT,
 		IMPORT_AS_MONO,
-		//	IMPORT_ALL,
-		//	IMPORT_ALL_AS_MONO,
+		IMPORT_ALL,
+		IMPORT_ALL_AS_MONO,
 
 		NUM_OPTIONS
 	};
 
-	const char* const option_text[NUM_OPTIONS] = { "IMPORT", "IMPORT AS MONO" };//, "IMPORT ALL", "IMPORT ALL AS MONO" };
+	StringBuilderBase<63>str_;
+
+	const char* const option_text[NUM_OPTIONS] = { "IMPORT", "IMPORT AS MONO", "IMPORT ALL", "IMPORT ALL AS MONO" };
 
 	void import_callback(int option) {
+		int wavs_total_ = 0;
+		int wavs_imported_ = 0;
+		Disk *disk_ = settings_->disk();
+		bool as_mono = option == IMPORT_ALL || option == IMPORT_ALL_AS_MONO;
+
 		switch (option)
 		{
 		case IMPORT:
-			if (settings_->wavImporter().import(&settings_->disk()->file(), DiskNavigatorPage::curr_entry_path(), false)) {
+		case IMPORT_AS_MONO:
+			if (settings_->wavImporter().import(&disk_->file(), DiskNavigatorPage::curr_entry_path(), as_mono)) {
 				MessagePainter::show("FINISHED");
-				return;
+			} else {
+				MessagePainter::show("FAILED");
 			}
 			break;
-		case IMPORT_AS_MONO:
-			if (settings_->wavImporter().import(&settings_->disk()->file(), DiskNavigatorPage::curr_entry_path(), true)) {
-				MessagePainter::show("FINISHED");
-				return;
+		case IMPORT_ALL:
+		case IMPORT_ALL_AS_MONO:
+			disk_->entry().rewind();
+
+			while (disk_->entry().next_visible()) {
+				if (disk_->entry().is_dir() == false) {
+					++wavs_total_;
+					str_.write(DiskNavigatorPage::curr_path(), "/", disk_->entry().name());
+					if (settings_->wavImporter().import(&disk_->file(), str_.read(), as_mono)) {
+						++wavs_imported_;
+					}
+				}
 			}
+
+			MessagePainter::show(str_.write(wavs_imported_, "/", wavs_total_, " IMPORTED"));
 			break;
 		default:
 			break;
 		}
-
-		MessagePainter::show("FAILED");
 	}
 
 	void confirm() {
