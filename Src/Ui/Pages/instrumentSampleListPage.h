9b17ca1c0545bee917793415e2b9de60131ee563
joostbooy <joostbooy@gmail.com>
2024-09-01 12:41:04 +0200

    cleanup

--- Src/Ui/Pages/instrumentSampleListPage.h
+++ Src/Ui/Pages/instrumentSampleListPage.h
@@ -11,14 +11,13 @@ namespace InstrumentSampleListPage {
 
 	enum FooterOptions {
 		ADD,
-		ADD_ALL,
 		REMOVE,
 		CLEAR,
 		CLOSE,
 		NUM_OPTIONS
 	};
 
-	const char* const footer_option_text[NUM_OPTIONS] = { "ADD" };
+	const char* const footer_option_text[NUM_OPTIONS] = { "ADD", "REMOVE", "CLEAR", "CLOSE" };
 
 
 	void init() {
@@ -56,12 +55,6 @@ namespace InstrumentSampleListPage {
 					MessagePainter::show("SAMPLE ADDED");
 				}
 				break;
-			case ADD_ALL:
-				for (size_t i = 0; i < settings_->sampleAllocator().num_samples(); ++i) {
-					settings_->selected_instrument().add_sample(settings_->sampleAllocator().read_map(i));
-				}
-				MessagePainter::show("ALL SAMPLES ADDED");
-				break;
 			case REMOVE:
 				if (settings_->selected_instrument().remove_sample(settings_->selected_instrument_sample_index())) {
 					settings_->select_instrument_sample_index(settings_->selected_instrument_sample_index());
@@ -84,13 +77,14 @@ namespace InstrumentSampleListPage {
 
 	}
 
+	// Left : sample list -> Right : instrument sample list
 	void draw() {
-		//	for (int i = 0; i < n; ++i) {
-		//		canvas_->draw_text(x, y, settings_->sampleAllocator().read_map(i)->name());
-		//		if (i == settings_->selected_sample_index()) {
-		// hightlight
-		//		}
-		//	}
+	//	for (int i = 0; i < settings_->num_samples(); ++i) {
+	//		canvas_->draw_text(x, y, settings_->sampleAllocator().read_map(i)->name());
+	//		if (i == settings_->selected_sample_index()) {
+				// hightlight
+	//		}
+	//	}
 	}
 
 	const size_t target_fps() {
