df9f741a2942cfe5892c87f771c9af13befc7d9e
joostbooy <joostbooy@gmail.com>
2024-09-02 13:28:05 +0200

    more work sample list page

--- Src/Ui/Pages/instrumentSampleListPage.h
+++ Src/Ui/Pages/instrumentSampleListPage.h
@@ -8,6 +8,7 @@ namespace InstrumentSampleListPage {
 	using TopPage::settings_;
 	using TopPage::pages_;
 	using TopPage::canvas_;
+	using TopPage::leds_;
 
 	enum FooterOptions {
 		ADD,
@@ -19,6 +20,12 @@ namespace InstrumentSampleListPage {
 
 	const char* const footer_option_text[NUM_OPTIONS] = { "ADD", "REMOVE", "CLEAR", "CLOSE" };
 
+	void remove() {
+		if (settings_->selected_instrument().remove_sample(settings_->selected_instrument_sample_index())) {
+			settings_->select_instrument_sample_index(settings_->selected_instrument_sample_index());
+			MessagePainter::show("SAMPLE REMOVED");
+		}
+	}
 
 	void init() {
 
@@ -33,21 +40,15 @@ namespace InstrumentSampleListPage {
 	}
 
 	void on_encoder(int id, int state) {
-		settings_->select_sample_index(settings_->selected_sample_index() + state);
+		if (id == Controller::FUNCTION_ENC_A) {
+			settings_->select_sample_index(settings_->selected_sample_index() + state);
+		} else if (id == Controller::FUNCTION_ENC_D) {
+			settings_->select_instrument_sample_index(settings_->selected_instrument_sample_index() + state);
+		}
 	}
 
 	void on_button(int id, int state) {
 		if (state) {
-			if (id == Controller::UP_BUTTON) {
-				on_encoder(0, -1);
-				return;
-			}
-
-			if (id == Controller::DOWN_BUTTON) {
-				on_encoder(0, 1);
-				return;
-			}
-
 			switch (Controller::button_to_function(id))
 			{
 			case ADD:
@@ -56,13 +57,20 @@ namespace InstrumentSampleListPage {
 				}
 				break;
 			case REMOVE:
-				if (settings_->selected_instrument().remove_sample(settings_->selected_instrument_sample_index())) {
-					settings_->select_instrument_sample_index(settings_->selected_instrument_sample_index());
-					MessagePainter::show("SAMPLE REMOVED");
-				}
+				ConfirmationPage::set("REMOVE FROM LIST ?", [](int option) {
+					if (option == ConfirmationPage::CONFIRM) {
+						remove();
+					}
+				});
+				pages_->open(Pages::CONFIRMATION_PAGE);
 				break;
 			case CLEAR:
-				settings_->selected_instrument().clear_samples();
+				ConfirmationPage::set("CLEAR LIST ?", [](int option) {
+					if (option == ConfirmationPage::CONFIRM) {
+						settings_->selected_instrument().clear_samples();
+					}
+				});
+				pages_->open(Pages::CONFIRMATION_PAGE);
 				break;
 			case CLOSE:
 				pages_->close(Pages::INSTRUMENT_SAMPLE_LIST_PAGE);
@@ -74,17 +82,20 @@ namespace InstrumentSampleListPage {
 	}
 
 	void refresh_leds() {
-
+		leds_->set_footer_encoder(0, Leds::RED);
+		leds_->set_footer_encoder(1, Leds::BLACK);
+		leds_->set_footer_encoder(2, Leds::BLACK);
+		leds_->set_footer_encoder(3, Leds::RED);
 	}
 
 	// Left : sample list -> Right : instrument sample list
 	void draw() {
-	//	for (int i = 0; i < settings_->num_samples(); ++i) {
-	//		canvas_->draw_text(x, y, settings_->sampleAllocator().read_map(i)->name());
-	//		if (i == settings_->selected_sample_index()) {
-				// hightlight
-	//		}
-	//	}
+		//	for (int i = 0; i < settings_->num_samples(); ++i) {
+		//		canvas_->draw_text(x, y, settings_->sampleAllocator().read_map(i)->name());
+		//		if (i == settings_->selected_sample_index()) {
+		// hightlight
+		//		}
+		//	}
 	}
 
 	const size_t target_fps() {
