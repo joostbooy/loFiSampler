82f7377b4ea85756764a90f5509879dde5e24b86
joostbooy <joostbooy@gmail.com>
2024-09-04 11:23:13 +0200

    refactored wav import page

--- Src/Ui/Pages/wavImportPage.h
+++ Src/Ui/Pages/wavImportPage.h
@@ -7,6 +7,8 @@ namespace WavImportPage {
 
 	using TopPage::pages_;
 	using TopPage::settings_;
+	using TopPage::canvas_;
+	using TopPage::ui_;
 
 	enum Options {
 		IMPORT,
@@ -21,16 +23,22 @@ namespace WavImportPage {
 
 	const char* const option_text[NUM_OPTIONS] = { "IMPORT", "IMPORT AS MONO", "IMPORT ALL", "IMPORT ALL AS MONO" };
 
+	void draw_progression(const char *file_name) {
+		MessagePainter::show(str_.write("IMPORTING... ", file_name));
+		ui_->send_display();
+	}
+
 	void import_callback(int option) {
 		int wavs_total_ = 0;
 		int wavs_imported_ = 0;
 		Disk *disk_ = settings_->disk();
-		bool as_mono = option == IMPORT_AS_MONO || option == IMPORT_ALL_AS_MONO;
+		bool as_mono = (option == IMPORT_AS_MONO) || (option == IMPORT_ALL_AS_MONO);
 
 		switch (option)
 		{
 		case IMPORT:
 		case IMPORT_AS_MONO:
+			draw_progression(DiskNavigatorPage::curr_entry_name());
 			if (settings_->wavImporter().import(DiskNavigatorPage::curr_entry_path(), as_mono)) {
 				MessagePainter::show("FINISHED");
 			} else {
@@ -43,6 +51,8 @@ namespace WavImportPage {
 
 			while (disk_->entry().next_visible()) {
 				if (disk_->entry().is_dir() == false) {
+					draw_progression(disk_->entry().name());
+
 					++wavs_total_;
 					str_.write(DiskNavigatorPage::curr_path(), "/", disk_->entry().name());
 					if (settings_->wavImporter().import(str_.read(), as_mono)) {
@@ -58,19 +68,14 @@ namespace WavImportPage {
 		}
 	}
 
-	void confirm() {
-		OptionListPage::set_text(option_text);
-		OptionListPage::set_callback(import_callback);
-		OptionListPage::set_count(NUM_OPTIONS);
-		pages_->open(Pages::OPTION_LIST_PAGE);
-	}
-
 	void init() {
 		DiskNavigatorPage::init();
 	}
 
 	void enter() {
-		DiskNavigatorPage::set_callback(confirm);
+		DiskNavigatorPage::set_footer_text(option_text);
+		DiskNavigatorPage::set_num_footer_options(NUM_OPTIONS);
+		DiskNavigatorPage::set_footer_callback(import_callback);
 		DiskNavigatorPage::set_list_filter(Entry::WAV);
 		DiskNavigatorPage::enter();
 	}
