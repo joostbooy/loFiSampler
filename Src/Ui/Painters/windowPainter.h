a4f3c23e342d68fc7cbb6e7b74a7e66bbd3a54c3
joostbooy <joostbooy@gmail.com>
2024-09-04 08:24:42 +0200

    dont paint scrollbar if not needed

--- Src/Ui/Painters/windowPainter.h
+++ Src/Ui/Painters/windowPainter.h
@@ -12,19 +12,23 @@ public:
 	}
 
 	static void draw_vertical_scollbar(int x, int y, int w, int h, int top_item, int num_items, int visible_items) {
-		int bar_y = (top_item * h) / num_items;
-		int bar_h = (visible_items * h) / num_items;
+		if (num_items > visible_items) {
+			int bar_y = (top_item * h) / num_items;
+			int bar_h = (visible_items * h) / num_items;
 
-		canvas_->fill(x, y, w, h, Canvas::BLACK);
-		canvas_->fill(x + 2, y + bar_y, w - 4, bar_h, Canvas::WHITE);
+			canvas_->fill(x, y, w, h, Canvas::BLACK);
+			canvas_->fill(x + 2, y + bar_y, w - 4, bar_h, Canvas::WHITE);
+		}
 	}
 
 	static void draw_horizontal_scollbar(int x, int y, int w, int h, int top_item, int num_items, int visible_items) {
-		int bar_x = (top_item * w) / num_items;
-		int bar_w = (visible_items * w) / num_items;
+		if (num_items > visible_items) {
+			int bar_x = (top_item * w) / num_items;
+			int bar_w = (visible_items * w) / num_items;
 
-		canvas_->fill(x, y, w, h, Canvas::BLACK);
-		canvas_->fill(bar_x, y + 2, bar_w , h - 4, Canvas::WHITE);
+			canvas_->fill(x, y, w, h, Canvas::BLACK);
+			canvas_->fill(bar_x, y + 2, bar_w , h - 4, Canvas::WHITE);
+		}
 	}
 
 	static void draw_footer(const char* const *text, int num_options) {
